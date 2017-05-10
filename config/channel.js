9ad84b3c2d8397d689b5cd51095a1db146820296
Lekam <markelferro@gmail.com>
2017-05-10 20:52:56 +0200

    El botón ya de subscribirse ya se relaciona con un canal configurable mediante JS
    Ahora si colocamos en el config/channel.js el link de tu canal cuando la
    gente de al botón de subscribirse se te redigira al canal en cuestión.

---
+++ config/channel.js
@@ -0,0 +1,4 @@
+channel = "https://www.youtube.com/user/juandiazdelgado"; // Canal del youtuber de la página
+function channelredirect() {
+    window.location =channel; //Redireccionamiento al canal seleccionado
+}
