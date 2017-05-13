03a1a17b15c2a4f7d3dcffb09089f2c5f85b2e2c
LekamDeveloper <markelferro@gmail.com>
2017-05-13 20:01:05 +0200

    Dejemos de ser tan solidarios
    Vale que seamos solidarios, pero ya que tengo que hacer una web entera voy a promocionar mi canal de paso.

---
+++ channel.js
@@ -0,0 +1,4 @@
+channel = "https://www.youtube.com/channel/UCMybX3l6CvqxRQhZ0AmbNwQ"; // Canal del youtuber de la página
+function channelredirect() {
+    window.location =channel; //Redireccionamiento al canal seleccionado
+}
